a01ae7ce5140cb105dc9f6f55c1d657f1c99cb49
wangrongjun <15521302230@163.com>
2018-01-03 23:45:07 +0800

    完善CsvConverter

--- src/main/java/com/wangrj/java_lib/java_util/CsvConverter.java
+++ src/main/java/com/wangrj/java_lib/java_util/CsvConverter.java
@@ -3,7 +3,6 @@ package com.wangrj.java_lib.java_util;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,15 +15,39 @@ public class CsvConverter {
 
     private String columnSeparator = ";";
     private String rowSeparator = "\r\n";
-    private boolean ignoreCsvTextSyntaxError = true;
+
+    interface ValueConverter {
+        Object convert(Field field, Object value);
+    }
 
     public <T> String toCsvText(List<T> entityList) {
+        return toCsvText(entityList, (field, value) -> {
+            if (value instanceof Date) {// 如果value是Date的子类
+                Date date = (Date) value;
+                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+            }
+            return value.toString();
+        });
+    }
+
+    /**
+     * 把实体列表转换为Csv文本
+     * <p>
+     * Csv规则（参考百度百科：https://baike.baidu.com/item/CSV/10739）
+     * 1. 包含逗号, 双引号, 或是换行符的字段必须放在引号内
+     * 2. 字段内部的引号必须在其前面增加一个引号来实现文字引号的转码
+     * 3. 分隔符逗号前后的空格 可能不会 被修剪掉. 这是RFC 4180的要求
+     * 4. 元素中的换行符将被保留下来
+     *
+     * @param entityList
+     * @param converter
+     * @param <T>
+     * @return
+     */
+    public <T> String toCsvText(List<T> entityList, ValueConverter converter) {
         // 判断列表是否为空
         if (entityList == null || entityList.size() == 0) {
             return null;
-        }
-        if (entityList.size() == 0) {
-            return "";
         }
 
         // 初始化
@@ -38,6 +61,10 @@ public class CsvConverter {
                 Field field = fields[j];
                 field.setAccessible(true);
                 Object value = ReflectUtil.get(field, entity);
+                // 进行数据转换
+                if (converter != null) {
+                    value = converter.convert(field, value);
+                }
                 builder.append(value == null ? "" : value.toString());
                 if (j < fields.length - 1) {// 如果不是当前行的最后一个属性，就在属性值后面加列分隔符
                     builder.append(columnSeparator);
@@ -52,7 +79,21 @@ public class CsvConverter {
         return builder.toString();
     }
 
-    // ====================================== Test Separator ======================================
+    public <T> List<T> fromCsvText(String csvText) {
+        return null;
+    }
+
+    public CsvConverter setColumnSeparator(String columnSeparator) {
+        this.columnSeparator = columnSeparator;
+        return this;
+    }
+
+    public CsvConverter setRowSeparator(String rowSeparator) {
+        this.rowSeparator = rowSeparator;
+        return this;
+    }
+
+    // ====================================== 以下是测试代码 ======================================
 
     static class UserBean {
         enum Gender {
@@ -141,11 +182,6 @@ public class CsvConverter {
             userBeanList.add(user);
         }
         System.out.println(toCsvText(userBeanList));
-    }
-
-    public static void main(String[] args) throws ParseException {
-        Date date = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy").parse("Mon Oct 10 00:00:00 CST 2107");
-        System.out.println(date);
     }
 
 }

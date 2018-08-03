76b20c2161cdd447686e80f773ec78b40d4a9237
robin.wang <robin.wang@bamboosnetworks.com>
2018-08-03 12:18:44 +0800

    开发SearchFile小程序

---
+++ src/main/java/com/wangrj/java_lib/java_program/SearchFile.java
@@ -0,0 +1,77 @@
+package com.wangrj.java_lib.java_program;
+
+import com.wangrj.java_lib.java_util.FileUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SearchFile {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入查询的目录：");
+        File folder = new File(scanner.next());
+        if (!folder.exists() || !folder.isDirectory()) {
+            System.out.println("错误：目录不存在或者不是目录");
+            return;
+        }
+        System.out.println("请输入搜索关键字：");
+        String searchWord = scanner.next();
+        scanner.close();
+
+        System.out.println("获取文件列表...");
+        List<File> children = FileUtil.findChildrenUnderDir(folder, null);
+        System.out.println("搜索中...");
+
+        List<String> searchRecordList = new ArrayList<>();
+        for (int i = 0; i < children.size(); i++) {
+            File file = children.get(i);
+            showText((i + 1) + " / " + children.size());
+            if (file.isDirectory()) {
+                continue;
+            }
+            String searchRecord = "";
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            int lineNumber = 0;
+            while ((line = br.readLine()) != null) {
+                lineNumber++;
+                if (line.contains(searchWord)) {
+                    searchRecord += "    " + lineNumber + ". " + line + "\r\n";
+                }
+            }
+            br.close();
+            fr.close();
+            if (searchRecord.length() > 0) {// 如果这个文件找到searchWord
+                searchRecord = file.getAbsolutePath() + "\r\n\r\n" + searchRecord;
+                searchRecordList.add(searchRecord);
+            }
+        }
+
+        System.out.println();
+        if (searchRecordList.size() > 0) {
+            System.out.println("搜索完成，以下是搜索记录：\r\n");
+        } else {
+            System.out.println("搜索完成，无搜索记录。");
+        }
+
+        for (int i = 0; i < searchRecordList.size(); i++) {
+            String searchRecord = searchRecordList.get(i);
+            System.out.println((i + 1) + ". " + searchRecord);
+        }
+    }
+
+    private static void showText(String text) {
+        System.out.print(text);
+        for (int i = 0; i < text.length(); i++) {
+            System.out.print("\b");
+        }
+    }
+
+}
--- src/main/java/com/wangrj/java_lib/test/JavaLibTestClass.java
+++ src/main/java/com/wangrj/java_lib/test/JavaLibTestClass.java
@@ -1,13 +1,30 @@
 package com.wangrj.java_lib.test;
 
-import java.io.File;
-
 public class JavaLibTestClass {
 
-    public static void main(String[] args) {
-        String s = "abc/aaabc\\";
-        String s1 = s.replaceFirst("abc[/|\\\\]", "def\\\\");
-        System.out.println(s1);
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("kkk");
+        System.out.print("1 / 12");
+        Thread.sleep(1000);
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("\b");
+        System.out.print("2 / 12");
+    }
+
+    public static void main1(String[] args) {
+        System.out.print("正在进行第 ");
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print("\b\b\b\b" + i + " 次");
+                Thread.currentThread().sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }

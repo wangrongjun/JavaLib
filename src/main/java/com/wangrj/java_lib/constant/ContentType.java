f17766c5a5e264a759aaa3f7786adcf79b94e405
wangrongjun <15521302230@163.com>
2018-08-14 22:50:34 +0800

    定义ContentType

---
+++ src/main/java/com/wangrj/java_lib/constant/ContentType.java
@@ -0,0 +1,13 @@
+package com.wangrj.java_lib.constant;
+
+/**
+ * by wangrongjun on 2018/8/14.
+ */
+public class ContentType {
+
+    public static final String TEXT_PLAIN = "text/plain;charset=UTF-8";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+    public static final String APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+}

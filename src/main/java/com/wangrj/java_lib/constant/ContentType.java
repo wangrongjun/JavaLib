51cfdd3074b133dec2640277b52caecb438a8ee8
wangrongjun <15521302230@163.com>
2018-10-07 11:49:59 +0800

    ContentType 增加 APPLICATION_JSON_UTF8

--- src/main/java/com/wangrj/java_lib/constant/ContentType.java
+++ src/main/java/com/wangrj/java_lib/constant/ContentType.java
@@ -7,6 +7,7 @@ public class ContentType {
 
     public static final String TEXT_PLAIN = "text/plain;charset=UTF-8";
     public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_JSON_UTF8 = "application/json;charset=UTF-8";
     public static final String MULTIPART_FORM_DATA = "multipart/form-data";
     public static final String APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
 
